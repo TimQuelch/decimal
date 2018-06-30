91d8c6f62e0e1fdf8b72e2d8d0e1cfef6526d560
Tim Quelch <tim@quelch.name>
2018-06-30 15:24:51 +1000

    Make everything constexpr

--- include/decimal.h
+++ include/decimal.h
@@ -22,49 +22,49 @@ namespace decimal {
         T scaled_;
 
     public:
-        decimal()
+        constexpr decimal()
             : scaled_{0} {}
 
         template <typename Numeric, typename = std::enable_if_t<std::is_arithmetic<Numeric>::value>>
-        decimal(Numeric value)
+        constexpr decimal(Numeric value)
             : scaled_{static_cast<T>(value) * scalar_} {}
 
         template <typename Numeric, typename = std::enable_if_t<std::is_arithmetic<Numeric>::value>>
-        explicit operator Numeric() const {
+        explicit constexpr operator Numeric() const {
             return static_cast<Numeric>(scaled_ / scalar_);
         }
 
-        decimal& operator+=(decimal rhs) {
+        constexpr decimal& operator+=(decimal rhs) {
             scaled_ += rhs.scaled_;
             return *this;
         }
 
-        decimal& operator-=(decimal rhs) {
+        constexpr decimal& operator-=(decimal rhs) {
             scaled_ -= rhs.scaled_;
             return *this;
         }
 
-        decimal& operator*=(decimal rhs) {
+        constexpr decimal& operator*=(decimal rhs) {
             scaled_ *= rhs.scaled_;
             return *this;
         }
 
-        decimal& operator/=(decimal rhs) {
+        constexpr decimal& operator/=(decimal rhs) {
             scaled_ /= rhs.scaled_;
             return *this;
         }
 
-        friend decimal operator+(decimal lhs, decimal rhs) { return lhs += rhs; }
-        friend decimal operator-(decimal lhs, decimal rhs) { return lhs -= rhs; }
-        friend decimal operator*(decimal lhs, decimal rhs) { return lhs *= rhs; }
-        friend decimal operator/(decimal lhs, decimal rhs) { return lhs /= rhs; }
+        friend constexpr decimal operator+(decimal lhs, decimal rhs) { return lhs += rhs; }
+        friend constexpr decimal operator-(decimal lhs, decimal rhs) { return lhs -= rhs; }
+        friend constexpr decimal operator*(decimal lhs, decimal rhs) { return lhs *= rhs; }
+        friend constexpr decimal operator/(decimal lhs, decimal rhs) { return lhs /= rhs; }
 
-        friend bool operator==(decimal lhs, decimal rhs) { return lhs.scaled_ == rhs.scaled_; }
-        friend bool operator!=(decimal lhs, decimal rhs) { return !(lhs == rhs); }
-        friend bool operator<(decimal lhs, decimal rhs) { return lhs.scaled_ < rhs.scaled_; }
-        friend bool operator>(decimal lhs, decimal rhs) { return rhs < lhs; }
-        friend bool operator<=(decimal lhs, decimal rhs) { return !(lhs > rhs); }
-        friend bool operator>=(decimal lhs, decimal rhs) { return !(lhs < rhs); }
+        friend constexpr bool operator==(decimal lhs, decimal rhs) { return lhs.scaled_ == rhs.scaled_; }
+        friend constexpr bool operator!=(decimal lhs, decimal rhs) { return !(lhs == rhs); }
+        friend constexpr bool operator<(decimal lhs, decimal rhs) { return lhs.scaled_ < rhs.scaled_; }
+        friend constexpr bool operator>(decimal lhs, decimal rhs) { return rhs < lhs; }
+        friend constexpr bool operator<=(decimal lhs, decimal rhs) { return !(lhs > rhs); }
+        friend constexpr bool operator>=(decimal lhs, decimal rhs) { return !(lhs < rhs); }
     };
 } // namespace decimal
 
