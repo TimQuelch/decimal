b020cefd918b3b1241c121fb6ca12e0119fcac51
Tim Quelch <tim@quelch.name>
2019-07-11 08:49:35 +1000

    Set up initial packaging with conan

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,25 @@
-from conans import ConanFile
+from conans import ConanFile, tools, CMake
 
 class SeqslamBenchmarkConan(ConanFile):
-    requires = ("Catch2/2.7.1@catchorg/stable")
     generators = ("cmake_paths")
+
+    def build_requirements(self):
+        if tools.get_env("CONAN_RUN_TESTS", True):
+            self.build_requires("Catch2/2.7.1@catchorg/stable")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["DECIMAL_ENABLE_TESTING"] = "ON" if tools.get_env("CONAN_RUN_TESTS", True) else "OFF"
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = _configure_cmake(self)
+        cmake.build()
+        if tools.get_env("CONAN_RUN_TESTS", True):
+            cmake.test()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
